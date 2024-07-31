87b5fe62abd6b66ba5e69c7e06920ec47b3539c5
Dawson Mai <dawsonmmai@gmail.com>
2024-07-31 14:59:46 -0700

    Programmed calculator to test

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+    return array.reduce((total, current) => total + current, 0);
 };
 
 const multiply = function() {
-
+    return array.reduce((product, current) => product*current, 0);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	let fact = 1;
+  while(n>0){
+    fact *=n;
+    n--;
+  }
+  return fact;
 };
 
 // Do not edit below this line
